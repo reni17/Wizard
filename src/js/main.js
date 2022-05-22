381dc4790456b2f608c794e0b9bf383b396abc97
reni17 <renit00@mail.bg>
2022-05-22 17:38:17 +0300

    add game score

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 const startScreen = document.querySelector('.start-game');
-const gameScreen = document.querySelector('.game-screen')
+const gameScreen = document.querySelector('.game-screen');
+const gameScore = document.querySelector('.game-score');
 
 startScreen.addEventListener('click', (e)=> {
     e.currentTarget.classList.add('hidden')
     gameScreen.classList.remove('hidden')
+    gameScore.classList.remove('hidden')
 })
